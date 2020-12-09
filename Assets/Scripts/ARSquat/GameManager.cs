5ec217c1a7bdef3a8eaed7a4e7c00832b6053255
[ONEMOREDOT.]Sadahiro Takahashi <sada@onemoredot.com>
2020-12-10 05:30:19 +0900

    SquatTriggerとスクワット回数を処理するシングルトンを追加

---
+++ Assets/Scripts/ARSquat/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    static public GameManager instance;
+    private int score;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void CountUp()
+    {
+        Debug.Log("CountUp!");
+        score++;
+    }
+
+    public void CountClear()
+    {
+        Debug.Log("CountClear!");
+        score = 0;
+    }
+}
---
+++ Assets/Scripts/ARSquat/SquatTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class SquatTrigger : MonoBehaviour
+{
+    //OnTriggerEnter関数
+    void OnTriggerEnter(Collider other)
+    {
+        //接触したオブジェクトのタグが"Squat"のとき
+        if (other.CompareTag("Squat"))
+        {
+            Debug.Log("Enter");
+            GetComponent<Renderer>().material.color = Color.blue;
+            GameManager.instance.CountUp();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Squat"))
+        {
+            Debug.Log("Exit");
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+    }
+}
